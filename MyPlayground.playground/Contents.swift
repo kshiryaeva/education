15531fa1ffea9d6b2bd2c3fed50c6d70ed66ff07
k.shiryaeva <k.shiryaeva@macbook-k.shiryaeva>
2021-06-09 15:11:10 +0300

    Initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+/*let Name = "Ксения"
+let FirstName = "Ширяева"
+let Age = 24
+let Weight = 56
+
+
+print("Имя : \(Name)\nФамилия : \(FirstName)\nВозраст : \(Age)\nВес : \(Weight)")*/
+
+print(Int8.max)
+print(Int8.min)
+print(UInt8.max)
+print(UInt8.min)
+
+let  a : Int = 12
+let  b : Float = 15.5
+let  c : Double = 21.55
+
+let d = a+Int(b)+Int(c)
+print(d)
+
+let f = Float(a)+b+Float(c)
+print(f)
+
+let g = Double(a)+Double(b)+c
+print(g)
+
+let Result = true
+if Result {print("\nDouble more precisely")}
+else {print ("\nInt more precisely")}
